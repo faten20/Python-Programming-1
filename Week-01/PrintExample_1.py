e2300b9f9457bc6ab4e3d48da568fa888b5d8574
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-05 15:00:16 +0300

    Update PrintExample_1.py

--- Week-01/PrintExample_1.py
+++ Week-01/PrintExample_1.py
@@ -1,14 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
-#a Python program to print a string in a specific format 
+# Python program to print a string in a specific format 
 print("Welcome to Python Programming, \n\t Programming is fun!")
 
 
-# In[ ]:
+
 
 
 
