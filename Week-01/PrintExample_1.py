5b95f2e33d4de4a0193dce1ccb53e9dea8fe0a8e
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-05 14:54:10 +0300

    Add files via upload

---
+++ Week-01/PrintExample_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#a Python program to print a string in a specific format 
+print("Welcome to Python Programming, \n\t Programming is fun!")
+
+
+# In[ ]:
+
+
+
+
