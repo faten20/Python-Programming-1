647dc7165a977c1f06abd6ff1928428a1b7b83e7
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-06 15:33:07 +0300

    Update StringExample.py

--- Week_02/StringExample.py
+++ Week_02/StringExample.py
@@ -1,24 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[13]:
-
-
 #The use of format() method
 age = 35
 children = 4
 myinfo = "My name is Lila I am {} years old I have {} kids."
 print(myinfo.format(age, children))
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
