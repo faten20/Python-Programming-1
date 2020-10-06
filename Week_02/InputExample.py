4f5f760183f80b2878a7259c9cd95fbfa27c9dbb
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-06 15:30:47 +0300

    Add files via upload

---
+++ Week_02/InputExample.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#a Python program that request the user name and displays the name back in upper cases
+yourinput = input("What's your name? ")
+print("My name is ", yourinput.upper())
+
+
+# In[ ]:
+
+
+
+
---
+++ Week_02/StringExample.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+#The use of format() method
+age = 35
+children = 4
+myinfo = "My name is Lila I am {} years old I have {} kids."
+print(myinfo.format(age, children))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
