86e9fd6b8669feeb25a27665b8d741442dc5bc87
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-09 19:03:16 +0300

    Update FunctionEx-1.py

--- Week_5/FunctionEx-1.py
+++ Week_5/FunctionEx-1.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[22]:
-
-
 #Python function to find the Min of four numbers
 def min_of_two( x, y ):
     if x < y:
@@ -13,23 +7,10 @@ def min_of_three( x, y, z ):
     return min_of_two( x, min_of_two( y, z ) )
 def min_of_four( x, y, z,n ):
     return min_of_two( x, min_of_three( y, z ,n) )
-
 print(min_of_four(3, 2, 5,1))
 
 
-# In[ ]:
 
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
