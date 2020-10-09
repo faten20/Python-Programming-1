a42d74261b85ae8be6ae9ffaa3a486a3aa9a950d
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-09 12:50:11 +0300

    Update ForLoopExample.py

--- Week_4/ForLoopExample.py
+++ Week_4/ForLoopExample.py
@@ -1,25 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
 
 # Program to find the product of all numbers stored in a list
 
-
 numbers = [2,4,6,1]
-
-
 prod = 1
-
-# loop over the list
+  loop over the list
 for n in numbers:
 	prod = prod*n
-
 print("The product is", prod)
 
 
-# In[ ]:
+
 
 
 
