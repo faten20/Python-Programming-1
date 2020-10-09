6416da272f70d8414902d0291eaa1d5e3b1fb090
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-09 12:51:21 +0300

    Update WhileLoopExample.py

--- Week_4/WhileLoopExample.py
+++ Week_4/WhileLoopExample.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 # Prints all letters in the string "I learn python" except the letter 'I'  
 i = 0
 str = 'I learn python'
-
 while i < len(str): 
 	if str[i] == 'I': 
 		i += 1
@@ -16,7 +9,7 @@ while i < len(str):
 	i += 1
 
 
-# In[ ]:
+
 
 
 
