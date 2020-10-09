601abaa0f402119365c32f6996c5d48a8bb4e068
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-09 19:04:33 +0300

    Update FunctionEx-2.py

--- Week_5/FunctionEx-2.py
+++ Week_5/FunctionEx-2.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[8]:
-
 
 #Python program to print the odd numbers from a given list
 def isodd(list):
@@ -14,7 +9,7 @@ def isodd(list):
 print(isodd([1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12]))
 
 
-# In[ ]:
+
 
 
 
