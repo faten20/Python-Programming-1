de8cbb7146c6a64aa10ddd8ac48d4068227dba96
faten20 <52507139+faten20@users.noreply.github.com>
2020-10-17 13:53:14 +0100

    Add files via upload

---
+++ Week_8/DictionaryExample-1.py
@@ -0,0 +1,15 @@
+#creating changing and accessing elements from dictionary
+dict1 = {'name': 'Ali', 'age': 30,'address':'nw44uj','job':'accountant'}
+print(dict1['name'])
+print(dict1['age'])
+print(dict1['address'])
+print(dict1['job'])
+dict1['age'] = 20
+print(dict1['age'])
+
+dict1['location'] = 'London'
+
+print(dict1['location'])
+
+
+
---
+++ Week_8/DictionaryExample-2.py
@@ -0,0 +1,8 @@
+#iterate through key in a dictionary and dictionary functions example
+fact = {1: 1, 2: 2, 3: 6, 4: 24, 5: 120}
+for i in fact:
+    print(fact[i])
+
+print(len(fact))
+
+print(sorted(fact))
